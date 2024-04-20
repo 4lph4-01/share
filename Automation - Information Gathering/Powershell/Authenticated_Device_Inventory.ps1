82f1da2e94026d6f385d81e780534cdcddf78864
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2024-04-20 19:38:11 +0100

    Update Authenticated_Device_Inventory.ps1

--- Automation - Information Gathering/Powershell/Authenticated_Device_Inventory.ps1
+++ Automation - Information Gathering/Powershell/Authenticated_Device_Inventory.ps1
@@ -1,5 +1,5 @@
 ########################################################################################################################################################################################
-# Powershell automated Information Gathering Script By: 4lph4-01 for simulation 11/04/2024
+# Powershell automated Information Gathering Script By: 41ph4-01 for simulation 11/04/2024
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
