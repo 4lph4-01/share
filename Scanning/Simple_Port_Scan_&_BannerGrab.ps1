f9be957622cbe03dd02a6a7b9ff71bbd0022a579
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2024-04-20 23:56:29 +0100

    Update Simple_Port_Scan_&_BannerGrab.ps1

--- Scanning/Simple_Port_Scan_&_BannerGrab.ps1
+++ Scanning/Simple_Port_Scan_&_BannerGrab.ps1
@@ -1,7 +1,7 @@
 #########################################################################################################################################################################################
 # Simple Port Scan & Banner Grab
 # This scanner will perform an ARP scan to discover active hosts on the local network. 
-#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
 # to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial 
 # portions of the Software.
