98e2c0ead9d07d261937f187a40c006f0dceca2e
4lph4-01 <166868320+4lph4-01@users.noreply.github.com>
2024-04-20 18:18:47 +0100

    Update Basic_Web_Application_Query_OWASP_Top10.py

--- Web_Applications/Basic_Web_Application_Query_OWASP_Top10.py
+++ Web_Applications/Basic_Web_Application_Query_OWASP_Top10.py
@@ -7,7 +7,7 @@
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
 # AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# Lets work together to get the full top ten coded?
+# Replace /example_endpoint with the specific endpoints you want to test for each vulnerability. Replace "http://your_base_url" with the base URL of the application you're testing
 #########################################################################################################################################################################################
 
 
