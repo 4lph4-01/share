2e70d6e351f912e93e0f0f888eb53827a752b0dd
4lph4-01 <166868320+4lph4-01@users.noreply.github.com>
2024-04-20 17:09:02 +0100

    Update Authenticated_Device_Inventory.ps1

--- Automation - Information Gathering/Powershell/Authenticated_Device_Inventory.ps1
+++ Automation - Information Gathering/Powershell/Authenticated_Device_Inventory.ps1
@@ -1,7 +1,6 @@
 ########################################################################################################################################################################################
 # Powershell automated Information Gathering Script By: 4lph4-01 for simulation 11/04/2024
-# Bash script: Installs packages and binaries; Adjust the URL to run.
-# Eyewitness requires manual configuration, Dirctions to GitHub page for installation.
+# 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
 # to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial 
