f87750b2de915767c45fa36c225cdadd16cd0568
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2024-04-22 07:12:24 +0100

    Update Basic_Web_Application_Query_OWASP_Top10.py

--- Web_Applications/Basic_Web_Application_Query_OWASP_Top10.py
+++ Web_Applications/Basic_Web_Application_Query_OWASP_Top10.py
@@ -1,5 +1,5 @@
 #########################################################################################################################################################################################
-# Basic Python script example to look at a web application for possible vulnerabilities in a web application.
+# Basic Python script for possible vulnerabilities in a web application.
 # python vulnerability_tester.py
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
