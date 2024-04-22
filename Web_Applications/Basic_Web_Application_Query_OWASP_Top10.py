d85894289db091036103e06fda75ea15af89d024
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2024-04-22 20:03:57 +0100

    Update Basic_Web_Application_Query_OWASP_Top10.py

--- Web_Applications/Basic_Web_Application_Query_OWASP_Top10.py
+++ Web_Applications/Basic_Web_Application_Query_OWASP_Top10.py
@@ -1,5 +1,5 @@
-#########################################################################################################################################################################################
-# Basic Python script for possible vulnerabilities in a web application, and define the scope of allowed domains.
+######################################################################################################################################################################################################################
+# Basic Python script for possible vulnerabilities in a web application, and define the scope of allowed domains. The script is an intial test, and does not constitute or replace a robust vulnerability scanner "Yet"
 # python vulnerability_tester.py. Note, script require BeautifulSoup Ref:https://beautiful-soup-4.readthedocs.io/en/latest/. 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
@@ -9,7 +9,7 @@
 # AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # Replace /example_endpoint with the specific endpoints you want to test for each vulnerability. Replace "http://your_base_url" with the base URL of the application you're testing
-#########################################################################################################################################################################################
+######################################################################################################################################################################################################################
 
 import requests
 from bs4 import BeautifulSoup
