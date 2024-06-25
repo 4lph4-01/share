b275b10fb1f9354456eeb53d56b16b7138c22250
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2024-06-25 09:34:37 +0100

    Update wifitest.py
    Updated

--- Wireless/wifitest.py
+++ Wireless/wifitest.py
@@ -1,7 +1,16 @@
-# Run using python3 script.py: The script includes checking and installing necessary tools, setting the interface to monitor mode, 
+###########################################################################################################################################################################################################
+# Run using python3 wifitest.py: The script includes checking and installing necessary tools, setting the interface to monitor mode, 
 # scanning for networks, capturing handshakes, and cracking the handshake using a wordlist.
 # This script includes necessary error handling and guides users through the process with clear feedback. It provides an interactive menu for network scanning, handshake capturing, and password cracking, 
 # making it more user-friendly and functional for real-world use.
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
+# without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+# to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial 
+# portions of the Software.
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
+# AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+###########################################################################################################################################################################################################
 
 import subprocess
 import sys
