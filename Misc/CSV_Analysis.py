f6df1018526a3e6bbd4538b5f31d8c0a2efca4f9
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2024-06-26 12:11:02 +0100

    Update CSV_Analysis.py

--- Misc/CSV_Analysis.py
+++ Misc/CSV_Analysis.py
@@ -1,5 +1,5 @@
 #########################################################################################################################################################################################################################
-# Basic python script to make a comparison between a set of spreadsheets and highligh duplicates. The script will produce a DataFrame listing all CVEs found in both CSV files (under construction): 41ph4-01 01/06/2024 
+# Basic python script to make a comparison between a set of spreadsheets and highlight duplicates. The script will produce a DataFrame listing all CVEs found in both CSV files (under construction): 41ph4-01 01/06/2024 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
 # to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial 
