1e57040da9e5f8253eb490498d9f3154d0070793
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2024-07-21 11:13:01 +0100

    Update capture_and_pass_hash.py

--- Lateral_Movement/capture_and_pass_hash.py
+++ Lateral_Movement/capture_and_pass_hash.py
@@ -1,5 +1,6 @@
 ######################################################################################################################################################################################################################
-# Python script that installs required tools, checks for and attempts to install responder, hashcat, and crackmap, attemp[ts to hijack net-ntlm hashes off the network, correctly format the hashes gleened, using hashcat, gives a count of successfully recovered passwords, & converts them to NTLM.
+# Python script that installs required tools, checks for and attempts to install responder, hashcat, and crackmap, attempts to hijack net-ntlm hashes off the network attemps to recover using hashcat. Then correctly formats gleened representations to NTLM. 
+# And gives a count of successfully recovered passwords.
 # Bash: python3 capture_and_pass_hash.py. Ensure wordlist & rule file is accessible in the same folder, or specify the paths. Special thanks to StealthSploit for the robust rule file. https://github.com/stealthsploit/OneRuleToRuleThemStill
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
