ce7ff672908d39236cee8bce89278a7027cb2aaa
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2024-11-12 10:12:24 +0000

    Update email_address_harvester.py

--- Automation - Information Gathering/email_address_harvester.py
+++ Automation - Information Gathering/email_address_harvester.py
@@ -1,5 +1,5 @@
 #####################################################################################################################################################################
-# Initial python to search the internet and darkweb for exposed email addresses, associated with the supplied domain. Note: API's required, TOR installation and port, for additional dark web integration.
+# Initial python to search the internet and darkweb for exposed email addresses, associated with the supplied domain. Note: API's required, TOR installation and port, for additional dark web integration (optional).
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the       
 # Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, # and to permit persons to whom the Software is furnished to do
 # so, subject to the following conditions: The above copyright notice and this permission notice shall be
