f858309d5d23ea3bfddf8235d1ec2500098a829e
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2024-12-14 19:25:22 +0000

    Update web_security_framework.py

--- Frameworks/web_security_framework.py
+++ Frameworks/web_security_framework.py
@@ -152,7 +152,7 @@ def directory_traversal(url):
 # Function to print the main menu
 def print_main_menu():
     print("=" * 30)
-    print("Advanced Penetration Testing Framework")
+    print("Advanced Web Penetration Testing Framework")
     print("=" * 30)
     print("[1] Crawl Website and Extract Forms      [2] Brute Force Test (Optional)")
     print("[3] SQL Injection Test                   [4] XSS Test")
