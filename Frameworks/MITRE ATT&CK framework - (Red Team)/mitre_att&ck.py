7f200459c190c531a33be047ec28c62a79b39459
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-01-07 16:41:19 +0000

    Update mitre_att&ck.py

--- Frameworks/MITRE ATT&CK framework - (Red Team)/mitre_att&ck.py
+++ Frameworks/MITRE ATT&CK framework - (Red Team)/mitre_att&ck.py
@@ -19,7 +19,7 @@ from termcolor import colored
 
 # Disclaimer and Ethical Guidelines
 def display_disclaimer():
-    print(r"""
+    print r"""
 
    _____  .___________________________________    _______________________________  _________   ____  __. ___________                                                  __                  _____  ____.____   __________  ___ ___    _____           _______  ____ 
   /     \ |   \__    ___/\______   \_   _____/   /  _  \__    ___/\__    ___/  _ \ \_   ___ \ |    |/ _| \_   _____/____________     _____   ______  _  _____________|  | __             /  |  |/_   |    |  \______   \/   |   \  /  |  |          \   _  \/_   |
@@ -51,7 +51,7 @@ def display_disclaimer():
     You must have explicit permission from the owner of any system you target with this script.
     Unauthorized testing and exploitation of systems is illegal and unethical.
     Always ensure you have written consent before conducting any security testing.
-    """)
+    """
 
 
     print(splash)
