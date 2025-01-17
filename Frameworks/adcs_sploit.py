7b261abfb6d316d7588c17bfe992c224cbe5173f
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-01-17 16:47:01 +0000

    Update adcs_sploit.py

--- Frameworks/adcs_sploit.py
+++ Frameworks/adcs_sploit.py
@@ -1,5 +1,5 @@
 ######################################################################################################################################################################################################################
-# ACDS_Sploit an initial python script framework for Active Directory Certificate Services, Note: Be mindful of the scope of work, & rules of engagement.
+# ADCS_Sploit an initial python script framework for Active Directory Certificate Services, Note: Be mindful of the scope of work, & rules of engagement.
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
 # to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial 
