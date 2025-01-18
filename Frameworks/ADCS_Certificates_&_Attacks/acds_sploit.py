ae21bdafd9001a3df6a7295b28cbb0640b587a03
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-01-18 13:05:49 +0000

    Update acds_sploit.py

--- Frameworks/ADCS_Certificates_&_Attacks/acds_sploit.py
+++ Frameworks/ADCS_Certificates_&_Attacks/acds_sploit.py
@@ -1,6 +1,6 @@
 ######################################################################################################################################################################################################################
 # Initial python script Framework, that install dependencies for attacking Active Directory Certificate Services Note: Be mindful of the scope of work, & rules of engagement. 
-# Ensure .env is in the same directory, and set to true for dry run (sumulation mode (non distructive). Test in a secure and controlled environment to avoid any unintended consequences.
+# Ensure .env (hidden file in this directory) is in the same directory, and set to true for dry run (sumulation mode (non distructive). Test in a secure and controlled environment to avoid any unintended consequences.
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
 # to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial 
