efc91a2324ebd862295e24c07409d4fa93b145fd
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-01-20 16:28:17 +0000

    Update web_security_framework.py

--- Frameworks/web_security_framework.py
+++ Frameworks/web_security_framework.py
@@ -1,6 +1,6 @@
 ######################################################################################################################################################################################################################
 # Python script for possible vulnerabilities in a web application, and does not constitute or replace a robust vulnerability scanner or manual testing. Note: Be mindful of the scope of work, & rules of engagement, script also requires BeautifulSoup Ref:https://beautiful-soup-4.readthedocs.io/en/latest/. .
-# python web_security_framework.py. Requires a linux virtual environment for older python version funtionality, pip restrictions for external dependancies, and reducing conflicts. 
+# python web_security_framework.py. Requires a linux virtual environment for older python version funtionality, pip restrictions for external dependancies, and reducing conflicts. Placeholders now in place for payload lists.
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
 # to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial 
