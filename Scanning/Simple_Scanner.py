7cf84b0260abc706b987be9074e7ed453486f417
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-01-22 14:42:43 +0000

    Update Simple_Scanner.py
    By 41ph4-01 23/04/2024 & our community.

--- Scanning/Simple_Scanner.py
+++ Scanning/Simple_Scanner.py
@@ -1,5 +1,5 @@
 #########################################################################################################################################################################################
-# In a python environment: python scanner.py IPADDRESS AND CIDR/SUBNET HERE
+# In a python environment: python scanner.py IPADDRESS AND CIDR/SUBNET HERE. By 41ph4-01 23/04/2024 & our community. 
 # This scanner will perform an ARP scan to discover active hosts on the local network. 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
