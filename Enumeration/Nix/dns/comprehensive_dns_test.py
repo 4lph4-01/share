9e9fbfccb5acc5c27d9e0edf500260a91be4d3b4
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-01-22 14:28:47 +0000

    Update comprehensive_dns_test.py

--- Enumeration/Nix/dns/comprehensive_dns_test.py
+++ Enumeration/Nix/dns/comprehensive_dns_test.py
@@ -1,5 +1,5 @@
 ######################################################################################################################################################################## 
-# A comprehensive python script to perform enumeration of DNS (Domain Name Services) and testing. Note: Requires a word list to dicover subdomains through bruteforce. From our community!
+# A comprehensive python script to perform enumeration of DNS (Domain Name Services) and testing. Note: Requires a word list to dicover subdomains through bruteforce. By 41ph4-01 23/04/2024 & our community. 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the       
 # Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, # and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
