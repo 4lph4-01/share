0dda641185fc6abbc69d7efc3bd2ee3c9089a0e1
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-01-23 12:46:21 +0000

    Update ipid_idle_scan.py
    Banner updated.

--- Scanning/ipid_idle_scan.py
+++ Scanning/ipid_idle_scan.py
@@ -1,3 +1,15 @@
+#########################################################################################################################################################################################
+# Python IPID Idle scan based on inputs. Uses the scapy library for packet crafting and analysis. NOTE: Scapy requires root access to craft raw packets.  By 41ph4-01 23/04/2024 & our community. 
+# This scanner will perform an ARP scan to discover active hosts on the local network. 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
+# without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+# to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial 
+# portions of the Software.
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
+# AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#########################################################################################################################################################################################
+
 from scapy.all import *
 import re
 
