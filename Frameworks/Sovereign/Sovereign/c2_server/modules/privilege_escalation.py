5cd36e75c848ffa0a019bcd155c7783d68442af8
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-01-26 00:22:15 +0000

    Delete Frameworks/Sovereign directory
    Duplicates

--- Frameworks/Sovereign/Sovereign/c2_server/modules/privilege_escalation.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def escalate_privileges():
-    if os.name == 'nt':
-        command = "powershell -Command \"Start-Process cmd -Verb runAs\""
-        os.system(command)
--- Frameworks/Sovereign/c2_server/modules/privilege_escalation.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def escalate_privileges():
-    # Placeholder for privilege escalation logic
-    if os.name == 'nt':
-        # Example: Attempt to escalate privileges using a known vulnerability
-        command = "powershell -Command \"Start-Process cmd -Verb runAs\""
-        os.system(command)
