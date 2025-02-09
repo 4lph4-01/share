86ccafd88752f220bec710776f8e0ef551311f49
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-02-09 22:11:51 +0000

    Update listener.sh

--- Frameworks/Sovereign-C2 (Enhanced) Under Testing/listeners/src/listener.sh
+++ Frameworks/Sovereign-C2 (Enhanced) Under Testing/listeners/src/listener.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 ######################################################################################################################################################################################################################
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
@@ -9,4 +8,5 @@
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ######################################################################################################################################################################################################################
 
+#!/bin/bash
 echo 'Listener script running...'
