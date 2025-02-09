1642b64dad88aedf3183a40750712d7d2dbfd509
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-02-09 15:53:18 +0000

    Update agent.ps1

--- Frameworks/Sovereign-C2 /agent/agent.ps1
+++ Frameworks/Sovereign-C2 /agent/agent.ps1
@@ -1,4 +1,5 @@
 ######################################################################################################################################################################## 
+# By 41lph4-01 and our community. 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”).
 # Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
 # and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be
