432da4b3a2106a06267a22e9407e8b6d7f077984
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-07-06 06:54:56 +0100

    Update invoke-derkerberoast.ps1

--- Frameworks/kerbroasting/powershell/invoke-derkerberoast.ps1
+++ Frameworks/kerbroasting/powershell/invoke-derkerberoast.ps1
@@ -1,7 +1,7 @@
 ########################################################################################################################################################################################
 # Powershell kerbroasting Script by: 41ph4-01 for simulation 11/04/2024
 # Enumerates SPN accounts in the domain, then requests TGS tickets by forcing network connections to service hosts, reads the Kerberos ticket cache files, parses each ticket with a real recursive 
-# ASN.1 DER TLV parser implemented fully in PowerShell, Extracts username, realm, and encrypted ticket parts,o utputs hashcat-compatible cracking lines.
+# ASN.1 DER TLV parser in PowerShell, extracts username, realm, and encrypted ticket parts, outputs hashcat-compatible cracking lines.
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
 # without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
 # to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial 
