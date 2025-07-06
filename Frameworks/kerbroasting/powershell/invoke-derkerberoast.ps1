2885786ac12869ded4d2ec8559449c3cc4866191
41ph4-01 <166868320+4lph4-01@users.noreply.github.com>
2025-07-06 06:54:04 +0100

    Update invoke-derkerberoast.ps1

--- Frameworks/kerbroasting/powershell/invoke-derkerberoast.ps1
+++ Frameworks/kerbroasting/powershell/invoke-derkerberoast.ps1
@@ -1,3 +1,16 @@
+########################################################################################################################################################################################
+# Powershell kerbroasting Script by: 41ph4-01 for simulation 11/04/2024
+# Enumerates SPN accounts in the domain, then requests TGS tickets by forcing network connections to service hosts, reads the Kerberos ticket cache files, parses each ticket with a real recursive 
+# ASN.1 DER TLV parser implemented fully in PowerShell, Extracts username, realm, and encrypted ticket parts,o utputs hashcat-compatible cracking lines.
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software 
+# without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+# to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial 
+# portions of the Software.
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
+# AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+########################################################################################################################################################################################
+
 function Invoke-DerKerberoast {
     [CmdletBinding()]
     param (
